a1efe4a1ad7d23c3a3fbc859a8ea42012e5dfbcb
Shankar Kulumani <shanks.k@gmail.com>
2017-09-20 09:43:12 -0400

    Update HW3

--- Homework/HW3/prob7_rv.py
+++ Homework/HW3/prob7_rv.py
@@ -4,7 +4,12 @@
 
 import numpy as np
 from astro import time, tle, constants, kepler
+
+import matplotlib.pyplot as plt
 import pdb
+
+# open the output file
+ofile = open('./prob7_rv.txt', 'w')
 
 # get TLE
 epoch, _ = time.date2jd(2017, 9, 19, 19, 0, 0)
@@ -13,21 +18,38 @@ for sat in sats:
     # get RV vector
     sat.tle_update(epoch, mu=constants.earth.mu)
     
-    r_eci = sat.r_eci
-    v_eci = sat.v_eci
+    (a, h, period, sme, fpa, r_per, r_apo, r_ijk, v_ijk,
+        r_pqw, v_pqw, r_lvlh, v_lvlh, r, v, v_circ, v_esc,
+        E, M, n) = kepler.elp_orbit_el(sat.coe.p, sat.coe.ecc, sat.coe.inc, 
+                                sat.coe.raan, sat.coe.argp, sat.coe.nu, 
+                                constants.earth.mu)
+    
+    ofile.write('{:10.6f} {:10.6f} {:10.6f} {:10.6f} {:10.6f} {:10.6f}\n'.format(
+        r_lvlh[0],
+        r_lvlh[1],
+        r_lvlh[2],
+        v_lvlh[0],
+        v_lvlh[1],
+        v_lvlh[2]))
 
     # get orbital paramters
     param_string = kepler.orbit_el(sat.coe.p, sat.coe.ecc, sat.coe.inc, sat.coe.raan, sat.coe.argp, sat.coe.nu, constants.earth.mu)
 
     print(sat.satname, sat.satnum)
     print(param_string)
+    
+    # create the plot of the orbit in the PQW reference frame
+    pos_eci, sat_eci, pos_pqw, sat_pqw = kepler.conic_orbit(sat.coe.p, sat.coe.ecc, sat.coe.inc,
+                                             sat.coe.raan, sat.coe.argp, sat.coe.nu, sat.coe.nu)
 
-with open('./prob7_rv.txt', 'w') as ofile:
-    for sat in sats:
-        ofile.write('{:16.6f} {:16.6f} {:16.6f} {:16.6f} {:16.6f} {:16.6f}\n'.format(sat.r_eci[0],
-                                                 sat.r_eci[1],
-                                                 sat.r_eci[2],
-                                                 sat.v_eci[0],
-                                                 sat.v_eci[1],
-                                                 sat.v_eci[2]))
+    fig, ax = plt.subplots()
+    ax.plot(pos_pqw[:, 0], pos_pqw[:, 1])
+    ax.plot(sat_pqw[0], sat_pqw[1], 'bo', markersize=9)
+    ax.plot(0, 0, 'ko', markersize=20)
+    ax.set_title(sat.satname)
+    plt.axis('equal')
+    plt.grid()
+
+ofile.close()
+plt.show()
     
