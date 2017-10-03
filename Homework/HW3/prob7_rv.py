ab8e7346e9c965e58083a0a63aaaa6fdbbd178ac
Shankar Kulumani <shanks.k@gmail.com>
2017-10-02 20:37:19 -0400

    Add solution for prob 7

--- Homework/HW3/prob7_rv.py
+++ Homework/HW3/prob7_rv.py
@@ -10,6 +10,7 @@ import pdb
 
 # open the output file
 ofile = open('./prob7_rv.txt', 'w')
+answer_file = open('./prob7_solution.txt', 'w')
 
 # get TLE
 epoch, _ = time.date2jd(2017, 9, 19, 19, 0, 0)
@@ -34,6 +35,8 @@ for sat in sats:
 
     # get orbital paramters
     param_string = kepler.orbit_el(sat.coe.p, sat.coe.ecc, sat.coe.inc, sat.coe.raan, sat.coe.argp, sat.coe.nu, constants.earth.mu)
+    
+    answer_file.write('\n{} {}\n{}'.format(sat.satname, sat.satnum, param_string))
 
     print(sat.satname, sat.satnum)
     print(param_string)
@@ -47,9 +50,13 @@ for sat in sats:
     ax.plot(sat_pqw[0], sat_pqw[1], 'bo', markersize=9)
     ax.plot(0, 0, 'ko', markersize=20)
     ax.set_title(sat.satname)
+    ax.set_xlabel(r'$ \hat p (km)$')
+    ax.set_ylabel(r'$ \hat q (km)$')
     plt.axis('equal')
     plt.grid()
+    plt.savefig(str(sat.satnum) + '.pdf')
 
 ofile.close()
+answer_file.close()
 plt.show()
     
