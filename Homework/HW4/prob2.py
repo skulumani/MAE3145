f2f44ad39624a23fc112aff64fcc34ec676ef078
Shankar Kulumani <shanks.k@gmail.com>
2017-10-29 18:45:46 -0400

    Start of problem 2 solution

---
+++ Homework/HW4/prob2.py
@@ -0,0 +1,17 @@
+"""Script to solve Problem 2 HW 4 2017
+"""
+
+import numpy as np
+from astro import constants, kepler
+
+
+re = constants.earth.radius
+mu_e = constants.earth.mu
+
+r_p = 1000 + re
+ecc = 1.05
+nu = np.deg2rad(90)
+
+a, p = kepler.hyp_per2sma(r_p, ecc)
+# compute orbit properties
+kepler.orbit_el(p, ecc, 0, 0, 0, nu, mu_e, True)
