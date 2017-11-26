361d533474a3fed4acecf50f66ff963f4fe20f46
Shankar Kulumani <shanks.k@gmail.com>
2017-11-26 09:36:54 -0500

    Add predict script

--- Projects/solution/code/predict.py
+++ Projects/solution/code/predict.py
@@ -1,3 +1,73 @@
-"""Predict script
-
+"""Script to run Predict
 """
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from collections import defaultdict
+import numpy as np
+from astro import geodetic, time, planets, tle, constants, transform
+from kinematics import attitude
+import os
+import pdb
+
+# TODO: Check that anywhere you use ECI2ECEF that its correct
+deg2rad = np.pi / 180
+rad2deg = 180 / np.pi
+
+mu = constants.earth.mu
+re = constants.earth.radius
+eesqrd = constants.earth.eesqrd
+ee = constants.earth.ee
+
+# define the observing site
+site_lat = 38.925 * deg2rad
+site_lon = -77.057 * deg2rad
+site_alt = 0.054
+site_ecef = geodetic.lla2ecef(site_lat, site_lon, site_alt)
+
+# timespan
+jd_start, _ = time.date2jd(2017, 12,1,  0, 0, 0)  # time in UTC
+jd_end, _ = time.date2jd(2017, 12, 7, 0, 0, 0)
+jd_step = 0.25 / (24 * 60)
+jd_span = np.arange(jd_start, jd_end, jd_step)
+
+ifile = os.path.abspath('ALL_TLE.txt')
+ofile = os.path.abspath('SAT_OUT.txt')
+
+# get latest TLE for ISS
+tle.get_tle_spacetrack(ifile, 'testing')
+
+# loop over jd span
+site = defaultdict(list)
+site['lat'] = site_lat
+site['lon'] = site_lon
+site['alt'] = site_alt
+site['ecef'] = site_ecef
+
+for jd in jd_span:
+    gst, lst = time.gstlst(jd, site_lon)
+    # site_eci = geodetic.site2eci(site_lat, site_alt, lst)
+    # site_eci = attitude.rot3(gst).dot(site_ecef)
+    site_eci = transform.dcm_ecef2eci(jd).dot(site_ecef)
+
+    # test if site is in the dark
+    sun_eci, _, _ = planets.sun_earth_eci(jd)
+
+    # append site information to list
+    site['eci'].append(site_eci)
+    site['sun_eci'].append(sun_eci)
+    site['gst'].append(gst)
+    site['lst'].append(lst)
+
+# turn into numpy arrays
+for key, item in site.items():
+    site[key] = np.squeeze(np.array(item))
+
+# update the downloaded TLEs if necessary
+# now we have to read the TLE
+sats = tle.get_tle(ifile)
+
+# now propogate and the check if each satellite is visible
+for sat in sats:
+    sat.tle_update(jd_span)
+    sat.visible(site)
+    sat.output(ofile)
