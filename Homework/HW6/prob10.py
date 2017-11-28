ea53761b3631273661f140ad5a37bae2f898f318
Shankar Kulumani <shanks.k@gmail.com>
2017-11-28 07:01:49 -0500

    Add problem 10 solution

--- Homework/HW6/prob10.py
+++ Homework/HW6/prob10.py
@@ -33,8 +33,30 @@ dv3 = np.absolute(v2 - vb2)
 dv_bielliptical = dv1 + dv2 + dv3
 tof_bielliptical = np.pi * ( np.sqrt(ab1**3/mu) + np.sqrt(ab2**3/mu))
 
+print('V1 : {} km/sec'.format(v1))
+print('V2 : {} km/sec'.format(v2))
+print('\nBielliptical transfer')
+print('VT1a : {} km/sec'.format(vb1))
+print('DV1 : {} km/sec'.format(dv1))
+
+print('\nVT2a : {} km/sec'.format(vb1i))
+print('VT2b : {} km/sec'.format(vb2i))
+print('DV2 : {} km/sec'.format(dv2))
+
+print('\nVT3b : {} km/sec'.format(vb2))
+print('DV3 : {} km/sec'.format(dv3))
+
+print('\nTOF : {} sec'.format(tof_bielliptical))
+
 # hohmann transfer
 at, pt, ecct = kepler.perapo2aecc(r1, r2)
 vt1 = maneuver.vel_mag(r1, at, mu) 
 vt2 = maneuver.vel_mag(r2, at, mu)
 dv1, dv2, tof, _ = maneuver.hohmann(r1, r2, 0, 0, 0, np.pi, mu)
+
+print('\nHohmann Transfer')
+print('VT1 : {} km/sec'.format(vt1))
+print('VT2 : {} km/sec'.format(vt2))
+print('DV1 : {} km/sec'.format(dv1))
+print('DV2 : {} km/sec'.format(dv2))
+print('TOF : {} sec'.format(tof))
