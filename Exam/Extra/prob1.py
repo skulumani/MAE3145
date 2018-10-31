afe8fa4d32db0e79c8aa0811ce9c874c49a43248
Shankar Kulumani <shanks.k@gmail.com>
2018-10-30 21:17:08 -0400

    Finish problem 1 solution

--- Exam/Extra/prob1.py
+++ Exam/Extra/prob1.py
@@ -27,3 +27,29 @@ nu_2 = nu_2[nu_2 < 0][0]
 tof_h = kepler.tof_nu(p_h, ecc_h, nu_1, nu_2, mu)
 
 print("TOF of {} sec from {} deg to {} deg".format(tof_h, np.rad2deg(nu_1), np.rad2deg(nu_2)))
+
+# time of flight for elliptical orbit
+a_e, p_e, ecc_e = kepler.perapo2aecc(rp, ra)
+
+nu_1 = kepler.nu_solve(p_e, ecc_e, re)
+nu_1 = np.array(nu_1)
+nu_1 = nu_1[nu_1 > 0][0]
+
+nu_2 = np.deg2rad(180)
+
+tof_e = kepler.tof_nu(p_e, ecc_e, nu_1, nu_2, mu)
+print("TOF of {} sec from {} deg to {} deg".format(tof_e[0], np.rad2deg(nu_1), np.rad2deg(nu_2)))
+
+# velocity at intercept
+
+# hyperbolic velocity at missile apogee
+sme_h = -mu / 2 / a_h
+sme_e = -mu / 2 / a_e
+
+vi_h = np.sqrt(2*(sme_h + mu / ra))
+vi_e = np.sqrt(2*(sme_e + mu / ra))
+
+print('Missile velocity = {} km/sec at {} km'.format(vi_e, ra))
+print('Body velocity = {} km/sec at {} km'.format(vi_h, ra))
+print('Closing velocity = {} km/sec'.format(np.absolute(vi_h - vi_e)))
+
